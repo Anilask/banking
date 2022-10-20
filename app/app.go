f3b53cb02c7f373424e2829eb95855c72e786841
Anilask <anilask56@gmail.com>
2022-10-20 21:29:25 +0530

    added gorilla mux

--- app/app.go
+++ app/app.go
@@ -3,10 +3,21 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
-	http.HandleFunc("/customer", getAllCusters)
+	// 	http.HandleFunc("/customer", getAllCusters)
 
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	// 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	//mux := http.NewServeMux()
+	mux := mux.NewRouter()
+	// define routes
+	mux.HandleFunc("/customer", getAllCusters)
+
+	// Start the server
+
+	log.Fatal(http.ListenAndServe("localhost:8080", mux))
+
 }
